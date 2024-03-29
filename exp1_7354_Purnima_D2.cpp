b4bf6b04cc5bd15e9ac8295d4b1f3563e8ce8eea
purnima7354 <162013638+purnima7354@users.noreply.github.com>
2024-03-29 13:38:18 +0530

    Add files via upload

---
+++ exp1_7354_Purnima_D2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+		int main(){
+			int number , count = 0;
+;
+			
+			for (int i = 0; i < 5;i++){
+				cout<< "Enter the number" <<endl;
+			cin>>number;
+			
+			if(number %8 == 0){
+				count++;
+			}
+	}
+			
+		cout << "Total "<< count << " numbers are valid numbers." << endl;
+	
+
+			return 0;
+			}
+			
+		
+	
